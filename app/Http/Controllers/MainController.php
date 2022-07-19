02738484d6020dd5aadfb6f057eb9d7659857630
rajesh-mondal <rajesh01749@gmail.com>
2022-07-19 23:55:41 +0600

    data fetching and filtering from database using laravel query builder class

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
@@ -16,5 +17,13 @@ class MainController extends Controller
 
     function contact(){
         return view("contact");
+    }
+
+    function allPeople(){
+        return (DB::table('people')
+                ->where('id','>',1)
+                ->orderby('id','desc')
+                ->limit(3)
+                ->get('id','name'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/', function () {
 Route::get('/',[MainController::class,'main']);
 Route::get('/features',[MainController::class,'features']);
 Route::get('/contact',[MainController::class,'contact']);
+Route::get('/allpeople',[MainController::class,'allpeople']);
